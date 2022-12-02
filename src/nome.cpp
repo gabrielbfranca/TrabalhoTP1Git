9cf9cc0107a2cbdc578482129f2dcaf608747f60
werble7 <bravehenrique@gmail.com>
2022-12-02 10:50:42 -0300

    Validar dominio Nome

--- src/nome.cpp
+++ src/nome.cpp
@@ -1,9 +1,23 @@
 #include "nome.h"
+#include <regex>
 
 void Nome::setNome(string nome) {
+    validar(nome);
     this->nome = nome;
 };
 
 Nome::Nome() {
     this->nome = "";
 };
+
+void Nome::validar(string nome){
+    regex nomeModelo("([A-Z]{1})([a-z]{1,}) ([A-Z]{1})([a-z]{1,}) ([A-Z]{1})([a-z]{1,})");
+
+    if (!regex_match(nome, nomeModelo)){
+        throw invalid_argument("Nome inválido");
+    }
+
+    if (strlen(nome.c_str()) > 30){
+        throw invalid_argument("Tamanho inválido");
+    }
+}
